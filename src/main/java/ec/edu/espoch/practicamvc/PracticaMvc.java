c98bf64c8c0e58e530f0904e1b699595b349d622
Stalyn0101 <yixon11@gmail.com>
2025-02-05 13:33:58 -0500

    primer comentario

---
+++ src/main/java/ec/edu/espoch/practicamvc/PracticaMvc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practicamvc;
+
+/**
+ *
+ * @author USER
+ */
+public class PracticaMvc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
