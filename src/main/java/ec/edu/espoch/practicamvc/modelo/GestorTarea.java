dadb2c0d51b26c4aa28e826640e06c5ba893f98c
welinton-cool <welinton.sacon@espoch.edu.ec>
2025-02-06 10:55:18 -0500

    Modelo

---
+++ src/main/java/ec/edu/espoch/practicamvc/modelo/GestorTarea.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.practicamvc.modelo;
+
+/**
+ *
+ * @author HP
+ */
+public class GestorTarea {
+    
+    private final Tarea [] tareas;
+    private int contadorTareas;
+
+    public GestorTarea() {
+        tareas = new Tarea[10];
+        contadorTareas = 0;
+    }
+
+   
+        public void agregarTarea(int id, String titulo, String descripcion, boolean tareaCompletada) {
+    if (contadorTareas < tareas.length) {
+        tareas[contadorTareas] = new Tarea(id, titulo, descripcion);
+        System.out.println("Tarea añadida: " + id + " - " + titulo + " - " + descripcion + " - Completada: " + tareaCompletada);  // Espacios añadidos para mejor legibilidad
+        contadorTareas++;
+        } else {
+            System.out.println("No se pueden agregar mas tareas.");
+        }
+    }
+
+    public void isCompletada() {
+        System.out.println("Tareas Pendientes");
+        for (int i = 0; i < contadorTareas; i++) {
+            if (tareas[i].isCompletada()) {
+                System.out.println(i + "; " + tareas[i].getDescripcion());
+
+            }
+
+        }
+    }
+
+    public void isCompleta() {
+        System.out.println("Tareas Completadas");
+        for (int i = 0; i < contadorTareas; i++) {
+            if (!tareas[i].isCompletada()) {
+
+            } else {
+            }
+        }
+    }
+}
---
+++ src/main/java/ec/edu/espoch/practicamvc/modelo/Tarea.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.practicamvc.modelo;
+
+/**
+ *
+ * @author HP
+ */
+public class Tarea {
+    
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(int id, String titulo, String descripcion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+}
